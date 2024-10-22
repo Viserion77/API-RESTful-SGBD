0a300760ce09c39937c092f46193563053562088
Viserion77 <jeferson.alves@adapcon.com.br>
2024-10-22 19:50:12 +0000

    feat: :sparkles: Update GET endpoint for producers' interval awards to return structured min and max data

--- src/index.js
+++ src/index.js
@@ -9,22 +9,33 @@ const server = createServer((req, res) => {
   console.info(`${method} ${url}`)
 
   if (url === '/api/v1/producers/interval-awards' && method === 'GET') {
-    console.log(
-      SqlBricks
-        .select('*')
-        .from('producers')
-        .where(SqlBricks.isNotNull('max_previousWin'))
-        .toString())
-    const total = select(
+    const producers = select(
       SqlBricks
         .select('*')
         .from('producers')
         .where(SqlBricks.isNotNull('max_previousWin'))
         .toString()
     )
+
+    const min = []
+    const max = []
+    for (const producer of producers) {
+      min.push({
+        producer: producer.name,
+        interval: producer.min_interval,
+        previousWin: producer.min_previousWin,
+        followingWin: producer.min_followingWin
+      })
+      max.push({
+        producer: producer.name,
+        interval: producer.max_interval,
+        previousWin: producer.max_previousWin,
+        followingWin: producer.max_followingWin
+      })
+    }
  
     res.setHeader('Content-Type', 'application/json')
-    res.write(JSON.stringify(total))
+    res.write(JSON.stringify({ min, max }))
   }
   // Não responder a requisições que não sejam mapeadas
   res.end()
@@ -40,37 +51,6 @@ server.listen(3000, async () => {
     method: 'GET',
   })).json()
 
-  // result wants to be:
-  // {
-  //   "min": [
-  //     {
-  //       "producer": "Producer 1",
-  //       "interval": 1,
-  //       "previousWin": 2008,
-  //       "followingWin": 2009
-  //     },
-  //     {
-  //       "producer": "Producer 2",
-  //       "interval": 1,
-  //       "previousWin": 2018,
-  //       "followingWin": 2019
-  //     }
-  //   ],
-  //     "max": [
-  //       {
-  //         "producer": "Producer 1",
-  //         "interval": 99,
-  //         "previousWin": 1900,
-  //         "followingWin": 1999
-  //       },
-  //       {
-  //         "producer": "Producer 2",
-  //         "interval": 99,
-  //         "previousWin": 2000,
-  //         "followingWin": 2099
-  //       }
-  //     ]
-  // }
   console.log('GET:', result)
 })
 
