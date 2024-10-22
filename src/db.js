4864e8224b03deece600a6d572a86edf6d2b6e3f
Viserion77 <jeferson.alves@adapcon.com.br>
2024-10-22 20:22:28 +0000

    feat: :sparkles: Refactor runSeed function to initialize and populate producers table with calculated reward intervals

--- src/db.js
+++ src/db.js
@@ -18,21 +18,67 @@ export function select(query) {
 }
 
 export function runSeed(movies) {
-  database.exec(`DROP TABLE IF EXISTS producers`);
-  database.exec(`
-        CREATE TABLE producers(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL,
-          min_interval INTEGER,
-          min_previousWin INTEGER,
-          min_followingWin INTEGER,
-          max_interval INTEGER,
-          max_previousWin INTEGER,
-          max_followingWin INTEGER
-        ) STRICT
-      `);
+  initializeProducersTable();
 
-  const producers = movies.reduce((acc, movie) => {
+  populateProducersTable(movies);
+}
+
+function populateProducersTable(movies) {
+  const producers = aggregateProducers(movies);
+
+  insert({
+    table: 'producers',
+    items: Object.values(producers).map(calculateRewardIntervals)
+  });
+}
+
+function calculateRewardIntervals(producer) {
+  const rewardYears = producer.rewardYears.sort((a, b) => a - b);
+
+  const min = {
+    interval: null,
+    previousWin: null,
+    followingWin: null,
+  };
+  const max = {
+    interval: null,
+    previousWin: null,
+    followingWin: null,
+  };
+
+  for (let i = 0; i < rewardYears.length; i++) {
+    const interval = rewardYears[i + 1] - rewardYears[i];
+    if (Number.isNaN(interval)) {
+      continue;
+    }
+
+    if (interval < min.interval || i === 0) {
+      min.interval = interval;
+      min.previousWin = rewardYears[i];
+      min.followingWin = rewardYears[i + 1];
+    }
+
+    if (interval > max.interval) {
+      max.interval = interval;
+      max.previousWin = rewardYears[i];
+      max.followingWin = rewardYears[i + 1];
+    }
+  }
+
+
+  return {
+    name: producer.name,
+    min_interval: min.interval,
+    min_previousWin: min.previousWin,
+    min_followingWin: min.followingWin,
+    max_interval: max.interval,
+    max_previousWin: max.previousWin,
+    max_followingWin: max.followingWin,
+  };
+}
+
+function aggregateProducers(movies) {
+  return movies.reduce((acc, movie) => {
     const producersNames = movie.producers.split(' and ');
 
     producersNames.forEach((producer) => {
@@ -52,52 +98,20 @@ export function runSeed(movies) {
 
     return acc;
   }, {});
-
-  insert({
-    table: 'producers',
-    items: Object.values(producers).map((producer) => {
-      const rewardYears = producer.rewardYears.sort((a, b) => a - b);
-
-      const min = {
-        interval: null,
-        previousWin: null,
-        followingWin: null,
-      }
-      const max = {
-        interval: null,
-        previousWin: null,
-        followingWin: null,
-      }
-
-      for (let i = 0; i < rewardYears.length; i++) {
-        const interval = rewardYears[i + 1] - rewardYears[i];
-        if (Number.isNaN(interval)) {
-          continue;
-        }
-
-        if (interval < min.interval || i === 0) {
-          min.interval = interval;
-          min.previousWin = rewardYears[i];
-          min.followingWin = rewardYears[i + 1];
-        }
-
-        if (interval > max.interval) {
-          max.interval = interval;
-          max.previousWin = rewardYears[i];
-          max.followingWin = rewardYears[i + 1];
-        }
-      }
-
-
-      return {
-        name: producer.name,
-        min_interval: min.interval,
-        min_previousWin: min.previousWin,
-        min_followingWin: min.followingWin,
-        max_interval: max.interval,
-        max_previousWin: max.previousWin,
-        max_followingWin: max.followingWin,
-      };
-    })
-  });
 }
+
+function initializeProducersTable() {
+  database.exec('DROP TABLE IF EXISTS producers');
+  database.exec(`
+    CREATE TABLE producers(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      min_interval INTEGER,
+      min_previousWin INTEGER,
+      min_followingWin INTEGER,
+      max_interval INTEGER,
+      max_previousWin INTEGER,
+      max_followingWin INTEGER
+    ) STRICT`);
+}
+
